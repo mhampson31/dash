2f2fe2311285e93c6531304d61d9ca612bb98591
Michael Hampson <toxotes+gh@gmail.com>
2022-03-13 12:06:45 -0400

    Tera template

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
 #[macro_use] extern crate rocket;
 
+use rocket_dyn_templates::Template;
+use rocket::serde::Serialize;
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct TemplateContext<'r> {
+    title: &'r str
+}
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    let context = TemplateContext {title: "Index"};
+    Template::render("index", context)
+}
+
+#[get("/test")]
+fn test() -> &'static str {
+    "This is a test route."
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/", routes![test])
+        .attach(Template::fairing())
 }
