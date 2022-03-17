06dd2cd068cc8a14efe4ed9a9786edde1b0a24b8
Michael Hampson <toxotes+gh@gmail.com>
2022-03-17 16:13:17 -0400

    Dummy data

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,52 @@
 #[macro_use] extern crate rocket;
 
-use rocket_dyn_templates::Template;
-use rocket::serde::Serialize;
+use rocket::serde::{Serialize, Deserialize};
 
-#[derive(Serialize)]
+use rocket_dyn_templates::Template;
+
+#[derive(Serialize, Deserialize)]
 #[serde(crate = "rocket::serde")]
-struct ServiceCard {
+struct Service {
     name: String,
     url: String,
     category: String,
     active: bool
 }
 
-
 #[derive(Serialize)]
 #[serde(crate = "rocket::serde")]
 struct TemplateContext<'r> {
     title: &'r str,
-    cards: Vec<ServiceCard>
+    services: Vec<Service>
 }
 
 #[get("/")]
 fn index() -> Template {
-    let mut cards:Vec<ServiceCard> = Vec::new();
-    cards.push(ServiceCard {
-        name: String::from("Foundry"),
-        url: String::from("sigil"),
-        category: String::from("Games"),
-        active: true
-    });
-    cards.push(ServiceCard {
-        name: String::from("Mealie"),
-        url: String::from("kitchen"),
-        category: String::from("Home"),
-        active: true
-    });
-    let context = TemplateContext {title: "Index", cards: cards};
-    Template::render("index", context)
-}
+    let mut services:Vec<Service> = Vec::new();
+    let snames = [
+        ("Foundry", "sigil", "Games", true),
+        ("Mealie", "kitchen", "Home", true),
+        ("Authentik", "id", "Admin", true),
+        ("Bookstack", "wiki", "Home", true)
+    ];
 
-#[get("/test")]
-fn test() -> &'static str {
-    "This is a test route."
+    for s in snames {
+        services.push(Service {
+            name: String::from(s.0),
+            url: String::from(s.1),
+            category: String::from(s.2),
+            active: s.3
+            }
+        )
+    }
+
+    let context = TemplateContext {title: "Index", services: services};
+    Template::render("index", context)
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index])
-        .mount("/", routes![test])
         .attach(Template::fairing())
 }
