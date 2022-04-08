d85242b242429cc71c7b83c877859420e60b56f9
Michael Hampson <toxotes+gh@gmail.com>
2022-04-08 18:23:03 -0400

    Basic list on /

--- src/main.rs
+++ src/main.rs
@@ -1,103 +1,212 @@
+mod entity;
 use axum::{
-    async_trait,
-    extract::{Extension, FromRequest, RequestParts},
+    extract::{Extension, Form, Path, Query},
     http::StatusCode,
-    routing::get,
-    Router,
+    response::Html,
+    routing::{get, get_service, post},
+    Router, Server,
 };
-use dotenv::dotenv;
-use sqlx::postgres::{PgPool, PgPoolOptions};
-use std::{net::SocketAddr, time::Duration};
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use axum_macros::debug_handler;
+use entity::service;
+use sea_orm::{prelude::*, Database, QueryOrder, Set};
+use serde::{Deserialize, Serialize};
+use service::Entity as Service;
+use std::str::FromStr;
+use std::{env, net::SocketAddr};
+use tera::Tera;
+use tower::ServiceBuilder;
+use tower_cookies::{CookieManagerLayer, Cookies};
+use tower_http::services::ServeDir;
 
 #[tokio::main]
-async fn main() {
-    dotenv().ok();
-    
-    tracing_subscriber::registry()
-        .with(tracing_subscriber::EnvFilter::new(
-            std::env::var("RUST_LOG").unwrap_or_else(|_| "example_tokio_postgres=debug".into()),
-        ))
-        .with(tracing_subscriber::fmt::layer())
-        .init();
+async fn main() -> anyhow::Result<()> {
+    env::set_var("RUST_LOG", "debug");
+    tracing_subscriber::fmt::init();
 
-    let db_connection_str = std::env::var("DB_URL")
-        .unwrap_or_else(|_| "postgres://postgres:password@localhost".to_string());
+    dotenv::dotenv().ok();
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    let host = env::var("HOST").expect("HOST is not set in .env file");
+    let port = env::var("PORT").expect("PORT is not set in .env file");
+    let server_url = format!("{}:{}", host, port);
 
-    print!("{}", &db_connection_str);
-
-    // setup connection pool
-    let pool = PgPoolOptions::new()
-        .max_connections(5)
-        .connect_timeout(Duration::from_secs(3))
-        .connect(&db_connection_str)
+    let conn = Database::connect(db_url)
         .await
-        .expect("can connect to database");
+        .expect("Database connection failed");
+    //Migrator::up(&conn, None).await.unwrap();
+    let templates = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/**/*"))
+        .expect("Tera initialization failed");
+    // let state = AppState { templates, conn };
 
-    // build our application with some routes
     let app = Router::new()
-        .route(
-            "/",
-            get(using_connection_pool_extractor).post(using_connection_extractor),
+        .route("/", get(list_services))
+        /*.route("/:id", get(edit_post).post(update_post))
+        .route("/new", get(new_post))
+        .route("/delete/:id", post(delete_post))*/
+        .nest(
+            "/static",
+            get_service(ServeDir::new(concat!(
+                env!("CARGO_MANIFEST_DIR"),
+                "/static"
+            )))
+            .handle_error(|error: std::io::Error| async move {
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!("Unhandled internal error: {}", error),
+                )
+            }),
         )
-        .layer(Extension(pool));
-
-    // run it with hyper
+        .layer(
+            ServiceBuilder::new()
+                .layer(CookieManagerLayer::new())
+                .layer(Extension(conn))
+                .layer(Extension(templates)),
+        );
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("listening on {}", addr);
-    axum::Server::bind(&addr)
+    Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
+
+    Ok(())
 }
 
-// we can extract the connection pool with `Extension`
-async fn using_connection_pool_extractor(
-    Extension(pool): Extension<PgPool>,
-) -> Result<String, (StatusCode, String)> {
-    sqlx::query_scalar("select 'hello world from pg'")
-        .fetch_one(&pool)
+#[derive(Deserialize)]
+struct Params {
+    page: Option<usize>,
+    posts_per_page: Option<usize>,
+}
+/*
+#[derive(Deserialize, Serialize, Debug, Clone)]
+struct FlashData {
+    kind: String,
+    message: String,
+}*/
+
+#[debug_handler]
+async fn list_services(
+    Extension(ref templates): Extension<Tera>,
+    Extension(ref conn): Extension<DatabaseConnection>,
+    Query(params): Query<Params>,
+) -> Result<Html<String>, (StatusCode, &'static str)> {
+    let services = Service::find()
+        .all(conn)
         .await
-        .map_err(internal_error)
+        .expect("could not retrieve services");
+
+    let mut ctx = tera::Context::new();
+    ctx.insert("service_list", &services);
+
+    //if let Some(value) = get_flash_cookie::<FlashData>(&cookies) {
+    //    ctx.insert("flash", &value);
+    //}
+
+    let body = templates
+        .render("index.html.tera", &ctx)
+        .map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, "Template error"))?;
+
+    Ok(Html(body))
+}
+/*
+async fn new_post(
+    Extension(ref templates): Extension<Tera>,
+) -> Result<Html<String>, (StatusCode, &'static str)> {
+    let ctx = tera::Context::new();
+    let body = templates
+        .render("new.html.tera", &ctx)
+        .map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, "Template error"))?;
+
+    Ok(Html(body))
 }
 
-// we can also write a custom extractor that grabs a connection from the pool
-// which setup is appropriate depends on your application
-struct DatabaseConnection(sqlx::pool::PoolConnection<sqlx::Postgres>);
+async fn create_post(
+    Extension(ref conn): Extension<DatabaseConnection>,
+    form: Form<post::Model>,
+    mut cookies: Cookies,
+) -> Result<PostResponse, (StatusCode, &'static str)> {
+    let model = form.0;
 
-#[async_trait]
-impl<B> FromRequest<B> for DatabaseConnection
-where
-    B: Send,
-{
-    type Rejection = (StatusCode, String);
-
-    async fn from_request(req: &mut RequestParts<B>) -> Result<Self, Self::Rejection> {
-        let Extension(pool) = Extension::<PgPool>::from_request(req)
-            .await
-            .map_err(internal_error)?;
-
-        let conn = pool.acquire().await.map_err(internal_error)?;
-
-        Ok(Self(conn))
+    post::ActiveModel {
+        title: Set(model.title.to_owned()),
+        text: Set(model.text.to_owned()),
+        ..Default::default()
     }
+    .save(conn)
+    .await
+    .expect("could not insert post");
+
+    let data = FlashData {
+        kind: "success".to_owned(),
+        message: "Post succcessfully added".to_owned(),
+    };
+
+    Ok(post_response(&mut cookies, data))
 }
 
-async fn using_connection_extractor(
-    DatabaseConnection(conn): DatabaseConnection,
-) -> Result<String, (StatusCode, String)> {
-    let mut conn = conn;
-    sqlx::query_scalar("select 'hello world from pg'")
-        .fetch_one(&mut conn)
+async fn edit_post(
+    Extension(ref templates): Extension<Tera>,
+    Extension(ref conn): Extension<DatabaseConnection>,
+    Path(id): Path<i32>,
+) -> Result<Html<String>, (StatusCode, &'static str)> {
+    let post: post::Model = Post::find_by_id(id)
+        .one(conn)
         .await
-        .map_err(internal_error)
+        .expect("could not find post")
+        .unwrap();
+
+    let mut ctx = tera::Context::new();
+    ctx.insert("post", &post);
+
+    let body = templates
+        .render("edit.html.tera", &ctx)
+        .map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, "Template error"))?;
+
+    Ok(Html(body))
 }
 
-/// Utility function for mapping any error into a `500 Internal Server Error`
-/// response.
-fn internal_error<E>(err: E) -> (StatusCode, String)
-where
-    E: std::error::Error,
-{
-    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
+async fn update_post(
+    Extension(ref conn): Extension<DatabaseConnection>,
+    Path(id): Path<i32>,
+    form: Form<post::Model>,
+    mut cookies: Cookies,
+) -> Result<PostResponse, (StatusCode, String)> {
+    let model = form.0;
+
+    post::ActiveModel {
+        id: Set(id),
+        title: Set(model.title.to_owned()),
+        text: Set(model.text.to_owned()),
+    }
+    .save(conn)
+    .await
+    .expect("could not edit post");
+
+    let data = FlashData {
+        kind: "success".to_owned(),
+        message: "Post succcessfully updated".to_owned(),
+    };
+
+    Ok(post_response(&mut cookies, data))
 }
+
+async fn delete_post(
+    Extension(ref conn): Extension<DatabaseConnection>,
+    Path(id): Path<i32>,
+    mut cookies: Cookies,
+) -> Result<PostResponse, (StatusCode, &'static str)> {
+    let post: post::ActiveModel = Post::find_by_id(id)
+        .one(conn)
+        .await
+        .unwrap()
+        .unwrap()
+        .into();
+
+    post.delete(conn).await.unwrap();
+
+    let data = FlashData {
+        kind: "success".to_owned(),
+        message: "Post succcessfully deleted".to_owned(),
+    };
+
+    Ok(post_response(&mut cookies, data))
+}*/
