3c4a15a2a1f52b0fe4332a141f95e8fdcb042ae3
Michael Hampson <toxotes+gh@gmail.com>
2022-04-08 12:44:44 -0400

    Axum + Sqlx example

--- src/main.rs
+++ src/main.rs
@@ -1,142 +1,103 @@
-#[macro_use]
-extern crate rocket;
+use axum::{
+    async_trait,
+    extract::{Extension, FromRequest, RequestParts},
+    http::StatusCode,
+    routing::get,
+    Router,
+};
+use dotenv::dotenv;
+use sqlx::postgres::{PgPool, PgPoolOptions};
+use std::{net::SocketAddr, time::Duration};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
-use rocket::http::{Cookie, CookieJar, SameSite};
-use rocket::response::Redirect;
-use rocket::serde::{Deserialize, Serialize};
-use rocket_dyn_templates::Template;
-use rocket_oauth2::{OAuth2, TokenResponse};
-use serde_yaml::from_str;
-use std::fs;
-//use rocket::http::hyper::Request;
-use reqwest::header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE};
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "example_tokio_postgres=debug".into()),
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
-// This struct will only be used as a type-level key. Multiple
-// instances of OAuth2 can be used in the same application by
-// using different key types.
-struct Authentik;
+    let db_connection_str = std::env::var("DB_URL")
+        .unwrap_or_else(|_| "postgres://postgres:password@localhost".to_string());
 
-//#[derive(serde::Deserialize)]
-struct User {
-    //    #[serde(default)]
-    name: String,
-}
+    print!("{}", &db_connection_str);
 
-/*
-#[rocket::async_trait]
-impl<'r> FromRequest<'r> for User {
-    type Error = std::convert::Infallible;
-
-    async fn from_request(request: &'r Request<'_>) -> request::Outcome<Self, Self::Error> {
-        request.cookies()
-            .get_private("user_id")
-            .and_then(|c| c.value().parse().ok())
-            .map(|id| User(id))
-            .or_forward(())
-    }
-}*/
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(crate = "rocket::serde")]
-struct Service {
-    name: String,
-    url: String,
-    category: String,
-    active: bool,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(crate = "rocket::serde")]
-struct ServiceList {
-    services: Vec<Service>,
-}
-
-#[derive(Serialize)]
-#[serde(crate = "rocket::serde")]
-struct TemplateContext<'r> {
-    title: &'r str,
-    service_list: ServiceList,
-}
-
-#[get("/")]
-async fn index(oauth2: OAuth2<Authentik>) -> Template {
-    oauth2.refresh(refresh_token).await.expect("Could not refresh token");
-    let config = fs::read_to_string("dash.yaml").expect("Failed to read input");
-    let services = from_str(&config).unwrap();
-
-    let context = TemplateContext {
-        title: "Index",
-        service_list: services,
-    };
-    Template::render("index", context)
-}
-
-#[get("/logout")]
-fn logout(cookies: &CookieJar) -> Redirect {
-    cookies.remove_private(Cookie::named("token"));
-    Redirect::to("/")
-}
-
-#[get("/message/<key>")]
-fn message(key: &str, cookies: &CookieJar) -> Option<String> {
-    cookies
-        .get_private(&key)
-        .map(|c| format!("Message: {}", c.value()))
-}
-
-// This route calls `get_redirect`, which sets up a token request and
-// returns a `Redirect` to the authorization endpoint.
-#[get("/login")]
-fn authentik_login(oauth2: OAuth2<Authentik>, cookies: &CookieJar<'_>) -> Redirect {
-    // We want the "user:read" scope. For some providers, scopes may be
-    // pre-selected or restricted during application registration. We could
-    // use `&[]` instead to not request any scopes, but usually scopes
-    // should be requested during registation, in the redirect, or both.
-    oauth2
-        .get_redirect(cookies, &["user:read", "email", "openid"])
-        .unwrap()
-}
-
-// This route, mounted at the application's Redirect URI, uses the
-// `TokenResponse` request guard to complete the token exchange and obtain
-// the token.
-//
-// The order is important here! If Cookies is positioned before
-// TokenResponse, TokenResponse will be unable to verify the token exchange
-// and return a failure.
-#[get("/auth/authentik")]
-async fn authentik_callback(token: TokenResponse<Authentik>, cookies: &CookieJar<'_>) -> Redirect {
-    // Set a private cookie with the access token
-    let client = reqwest::Client::new();
-    let request_url = "https://id.flyingseamonsters.com/api/v3/core/users/me/";
-    let response = client
-        .get(request_url)
-        .header(AUTHORIZATION, format!("Bearer {}", &token.access_token()))
-        .header(CONTENT_TYPE, "application/json")
-        .header(ACCEPT, "application/json")
-        .send()
+    // setup connection pool
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect_timeout(Duration::from_secs(3))
+        .connect(&db_connection_str)
         .await
-        .unwrap()
-        .text()
-        .await;
-    println!("{:?}", response);
+        .expect("can connect to database");
 
-    cookies.add_private(
-        Cookie::build("token", token.access_token().to_string())
-            .same_site(SameSite::Lax)
-            .finish(),
-    );
+    // build our application with some routes
+    let app = Router::new()
+        .route(
+            "/",
+            get(using_connection_pool_extractor).post(using_connection_extractor),
+        )
+        .layer(Extension(pool));
 
-    Redirect::to("/")
+    // run it with hyper
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build()
-        .mount(
-            "/",
-            routes![index, authentik_login, authentik_callback, message, logout],
-        )
-        .attach(OAuth2::<Authentik>::fairing("authentik"))
-        .attach(Template::fairing())
+// we can extract the connection pool with `Extension`
+async fn using_connection_pool_extractor(
+    Extension(pool): Extension<PgPool>,
+) -> Result<String, (StatusCode, String)> {
+    sqlx::query_scalar("select 'hello world from pg'")
+        .fetch_one(&pool)
+        .await
+        .map_err(internal_error)
+}
+
+// we can also write a custom extractor that grabs a connection from the pool
+// which setup is appropriate depends on your application
+struct DatabaseConnection(sqlx::pool::PoolConnection<sqlx::Postgres>);
+
+#[async_trait]
+impl<B> FromRequest<B> for DatabaseConnection
+where
+    B: Send,
+{
+    type Rejection = (StatusCode, String);
+
+    async fn from_request(req: &mut RequestParts<B>) -> Result<Self, Self::Rejection> {
+        let Extension(pool) = Extension::<PgPool>::from_request(req)
+            .await
+            .map_err(internal_error)?;
+
+        let conn = pool.acquire().await.map_err(internal_error)?;
+
+        Ok(Self(conn))
+    }
+}
+
+async fn using_connection_extractor(
+    DatabaseConnection(conn): DatabaseConnection,
+) -> Result<String, (StatusCode, String)> {
+    let mut conn = conn;
+    sqlx::query_scalar("select 'hello world from pg'")
+        .fetch_one(&mut conn)
+        .await
+        .map_err(internal_error)
+}
+
+/// Utility function for mapping any error into a `500 Internal Server Error`
+/// response.
+fn internal_error<E>(err: E) -> (StatusCode, String)
+where
+    E: std::error::Error,
+{
+    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
 }
